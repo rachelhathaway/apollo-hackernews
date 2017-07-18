18af978ad9a8def6c934efa7a28d26cda82bca27
rachelhathaway <rachel.e.hathaway@gmail.com>
2017-07-18 15:04:23 -0500

    configure auth token

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
 import { BrowserRouter } from 'react-router-dom';
+import { GC_AUTH_TOKEN } from './constants';
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj58oqaqzwttv0105hs39755v'
-});
+}).use([
+  {
+    applyMiddleware(req, next) {
+      const token = localStorage.getItem(GC_AUTH_TOKEN);
+      if (!req.options.headers) {
+        req.options.headers = {};
+      }
+      req.options.headers.authorization = token ? `Bearer ${token}` : null;
+      next();
+    }
+  }
+])
 
 const client = new ApolloClient({ networkInterface });
 
